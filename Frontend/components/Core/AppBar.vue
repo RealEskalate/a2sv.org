00ffb0a276d8808691220ba7f8026503c9662d1a
Beimnet Zewdu <zbeimnet@gmail.com>
2020-10-26 02:39:19 +0300

    [web] Support us page initial ui

--- Frontend/components/Core/AppBar.vue
+++ Frontend/components/Core/AppBar.vue
@@ -23,6 +23,11 @@ export default {
           title: 'Inspire',
           to: '/inspire',
         },
+        {
+          icon: 'mdi-chart-bubble',
+          title: 'Support Us',
+          to: '/support',
+        },
       ],
     }
   },

b3dc67d3c2d7f18b97195ed9e07a8b780d5e84b3
gemmechu <gemmechumohammed@gmail.com>
2020-10-26 17:40:49 +0300

    [web] inspire page recovered

--- Frontend/components/Core/AppBar.vue
+++ Frontend/components/Core/AppBar.vue
@@ -20,6 +20,11 @@ export default {
         },
         {
           icon: 'mdi-chart-bubble',
+          title: 'Inspire',
+          to: '/inspire',
+        },
+        {
+          icon: 'mdi-chart-bubble',
           title: 'Team',
           to: '/team',
         },

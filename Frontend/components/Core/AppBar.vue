dc463b76afe201b820c5cb881e03c793f96c27ff
gemmechu <gemmechumohammed@gmail.com>
2020-10-26 18:09:23 +0300

    [web] fix conflict

--- Frontend/components/Core/AppBar.vue
+++ Frontend/components/Core/AppBar.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-app-bar app color="white" class="shadow" flat>
+  <v-app-bar app color="white" elevate-on-scroll class="shadow">
     <v-tabs centered class="ml-n9" color="primary darken-1">
       <v-tab v-for="link in links" :key="link.to" :to="link.to">
         {{ link.title }}
@@ -24,9 +24,12 @@ export default {
           to: '/inspire',
         },
         {
-          icon: 'mdi-chart-bubble',
-          title: 'Team',
-          to: '/team',
+          title: 'Support Us',
+          to: '/support',
+        },
+        {
+          title: 'Events',
+          to: '/events',
         },
       ],
     }

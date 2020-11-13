910dbe6ce8ccd66b27371a2a1986887432065239
bontu <se.bontu.fufa@gmail.com>
2020-11-13 12:21:37 +0300

    [api] Adding team model, controller and route.

--- Backend/startup/routes.js
+++ Backend/startup/routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const indexRouter = require('../routes/indexRouter.js')
 const contactRouter = require('../routes/ContactControllerRouter.js')
+const teamRouter = require('../routes/TeamControllerRouter.js')
 const informationRouter = require('../routes/InformationRouter')
 const eventRouter = require('../routes/EventRouter')
 const mentorshipRouter = require("../routes/MentorshipRouter");
@@ -8,6 +9,7 @@ const questionAndAnswerRouter = require("../routes/QuestionAndAnswerRouter");
 
 router.use('/', indexRouter)
 router.use('/api/contact', contactRouter)
+router.use('/api/team', teamRouter)
 router.use('/api/mentorship', mentorshipRouter);
 router.use("/api/questionAndAnswer", questionAndAnswerRouter);
 router.use('/api/information/', informationRouter)

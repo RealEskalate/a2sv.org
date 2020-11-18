74a2e08e2e890739465d3ca92d1463630549c82c
Khalid-Sultan <Kalid.sultan.95@gmail.com>
2020-11-18 12:10:19 +0300

    Typo fix in Mentorship Controller

--- Backend/controllers/MentorshipController.js
+++ Backend/controllers/MentorshipController.js
@@ -31,7 +31,7 @@ exports.patchMentorships = async (req, res) => {
     const { id } = req.params;
     const mentorship = await Mentorship.findById(id)
     if (!mentorship) {
-        return res.status(404).send(`Event with id ${id} is not found`)
+        return res.status(404).send(`Mentorship with id ${id} is not found`);
     }
     mentorship.set(req.body)
     const notValid = validator(mentorship);

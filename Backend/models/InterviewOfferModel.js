d2e1ee52d6775ff3f6ccafe69752579ad382b94b
Nahusenay <nahusenay.yifter@gmail.com>
2020-11-20 20:20:37 +0300

    edited interview offer model

--- Backend/models/InterviewOfferModel.js
+++ Backend/models/InterviewOfferModel.js
@@ -1,6 +1,5 @@
 const mongoose = require('mongoose')
 const Joi = require('@hapi/joi')
-const { required } = require('@hapi/joi')
 
 const interviewOfferSchema = new mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,

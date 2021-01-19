79abf2a72bbdb6a654d3da1d28671187f0394f40
Segni Habulu <programmers2017@outlook.com>
2021-01-20 00:14:26 +0300

    Expose over PORT 3000

--- Frontend/nuxt.config.js
+++ Frontend/nuxt.config.js
@@ -57,5 +57,10 @@ export default {
   },
   loading: {
     color: "#00aeef"
+  },
+  server: {
+    port: 3000, // default: 3000
+    host: "0.0.0.0", // default: localhost,
+    timing: false
   }
 };

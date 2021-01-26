10ad58b4a8c17896ecf77d9afd371cdbb5b390d1
Nahusenay <nahusenay.yifter@gmail.com>
2021-01-26 14:53:49 +0300

    interview_offer removed

--- Backend/startup/routes.js
+++ Backend/startup/routes.js
@@ -3,7 +3,6 @@ const indexRouter = require('../routes/indexRouter.js')
 const contactRouter = require('../routes/ContactControllerRouter.js')
 const teamRouter = require('../routes/TeamControllerRouter.js')
 const informationRouter = require('../routes/InformationRouter')
-const interviewOfferRouter = require('../routes/InterviewOfferRouter')
 const eventRouter = require('../routes/EventRouter')
 const mentorshipRouter = require("../routes/MentorshipRouter");
 const questionAndAnswerRouter = require("../routes/QuestionAndAnswerRouter");
@@ -17,7 +16,6 @@ router.use('/api/team', teamRouter)
 router.use('/api/mentorship', mentorshipRouter);
 router.use("/api/questionAndAnswer", questionAndAnswerRouter);
 router.use('/api/information/', informationRouter)
-router.use('/api/interviewOffer/', interviewOfferRouter)
 router.use("/api/event/", eventRouter);
 router.use("/api/impactStoryResource/", impactStoryResourceRouter);
 router.use("/api/webResource/", webResourceRouter);

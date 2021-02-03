9206116390572f103984a1f8adca2a79bef450c9
Yisehak Bogale <yisehakb@gmail.com>
2021-02-03 19:00:01 +0300

    [frontend] Implement new layout for About page (#94)
    - Implement timeline view for phases
    - Improve the fonts and spacing of sections

--- Frontend/pages/about.vue
+++ Frontend/pages/about.vue
@@ -8,8 +8,8 @@
     />
     <v-container class="text-center mt-12 px-12">
       <div>
-        <h1 class="mb-8 sub-titles">
-          What we do
+        <h1 class="my-12 sub-titles">
+          What We Do
         </h1>
         <p
           v-if="getAboutUs"
@@ -52,114 +52,82 @@
           </div>
         </div>
       </v-lazy>
-
-      <div>
-        <h1 class="my-12 sub-titles">
-          How we do it
-        </h1>
-        <p
-          v-if="getAboutUs"
-          class="text-body"
-          v-html="getAboutUs.howWeDoIt.content"
-        />
-      </div>
-      <h1 v-if="getLearnPaths" class="display-1 my-6">
-        {{ getLearnPaths["Phase-1"].title }}
+    </v-container>
+    <v-container class="py-12 text-center">
+      <h1 class="my-12 sub-titles">
+        How We Do It
       </h1>
       <p
-        v-if="getLearnPaths"
-        class="justify-center text-body mb-10 text-center"
-        v-html="getLearnPaths['Phase-1'].subtitle"
+        v-if="getAboutUs"
+        class="text-body text-center"
+        v-html="getAboutUs.howWeDoIt.content"
       />
-      <carousel-3d
-        v-if="getLearnPaths"
-        style="min-height: 350px"
-        :border="0"
-        :perspective="20"
-        :inverse-scaling="50"
-        :space="350"
-        autoplay
-        :autoplay-timeout="5000"
-        width="400"
-      >
-        <slide
-          v-for="(item, i) in getLearnPaths['Phase-1'].list"
+
+      <v-timeline class="my-12">
+        <v-timeline-item
+          v-for="(phase, i) in getLearnPaths"
           :key="i"
-          class="overflow-visible"
-          :index="i"
-          style="height: auto; background-color: transparent"
+          class="d-flex align-center"
+          color="primary lighten-1"
+          fill-dot
+          small
         >
-          <v-card class="shadow" outlined shaped>
-            <v-img
-              class="white--text align-end"
-              height="200px"
-              :src="getFullPath(item.image)"
+          <div class="py-4" :class="`text-${ evenPhase(i) ? 'right' : 'left' }`" style="height: 100%">
+            <h1 v-if="getLearnPaths" class="font-weight-light primary--text">
+              {{ phase.title }}
+            </h1>
+            <v-divider class="primary lighten-5 my-4" :class="`float-${ evenPhase(i) ? 'right' : 'left' }`" style="width: 75px; border-width: 2px; border-radius: 2px" />
+            <br>
+            <br>
+            <p
+              class="text-body font-weight-light"
+              :class="`text-${ evenPhase(i) ? 'right' : 'left' }`"
+              v-html="phase.subtitle"
+            />
+          </div>
+
+          <template #opposite>
+            <carousel-3d
+              v-if="phase.list"
+              style="min-height: 400px"
+              :border="0"
+              :perspective="20"
+              :inverse-scaling="300"
+              :space="300"
+              autoplay
+              :display="3"
+              :autoplay-timeout="5000"
+              width="350"
             >
-              <v-card-title class="mr-3" v-text="item.title" />
-            </v-img>
-            <v-card-text v-text="item.content" />
-          </v-card>
-        </slide>
-      </carousel-3d>
-
-      <v-icon size="100" class="mb-5" style="margin-top: -20px">
-        {{ mdiArrowDownBoldHexagonOutline }}
-      </v-icon>
-
-      <h1 v-if="getLearnPaths" class="display-1 my-6">
-        {{ getLearnPaths["Phase-2"].title }}
-      </h1>
-      <p
-        v-if="getLearnPaths"
-        class="justify-center text-body mb-10"
-        v-html="getLearnPaths['Phase-2'].subtitle"
-      />
-      <carousel-3d
-        v-if="getLearnPaths"
-        style="min-height: 400px"
-        :border="0"
-        :perspective="20"
-        :inverse-scaling="50"
-        :space="350"
-        autoplay
-        :autoplay-timeout="5000"
-        width="400"
-      >
-        <slide
-          v-for="(item, i) in getLearnPaths['Phase-2'].list"
-          :key="i"
-          class="overflow-visible"
-          :index="i"
-          style="height: auto; background-color: transparent"
-        >
-          <v-card class="shadow" outlined shaped>
-            <v-img class="align-end" height="200px" :src="getFullPath(item.image)" />
-            <v-card-title class="primary--text pb-0" v-text="item.title" />
-            <v-card-text v-text="item.content" />
-          </v-card>
-        </slide>
-      </carousel-3d>
+              <slide
+                v-for="(item, j) in phase.list"
+                :key="j"
+                class="overflow-visible"
+                :index="j"
+                style="height: auto; background-color: transparent"
+              >
+                <v-card class="shadow-sm" shaped>
+                  <v-img style="opacity: 0.85" class="align-end" height="150px" :src="getFullPath(item.image)" />
+                  <v-card-title class="primary--text pb-2" v-text="item.title" />
+                  <v-card-text v-text="item.content" />
+                </v-card>
+              </slide>
+            </carousel-3d>
+            <v-img v-else src="illustrations/aboutus.svg" />
+          </template>
+        </v-timeline-item>
+      </v-timeline>
     </v-container>
 
-    <div class="grey lighten-4 mt-10 text-center">
+    <section class="grey lighten-4 mt-10 text-center">
       <Partners :message="message" />
-    </div>
-    <v-container>
-      <v-icon size="100" class="my-5 d-block mx-auto text-center">
-        {{ mdiArrowDownBoldHexagonOutline }}
-      </v-icon>
-      <div v-if="getLearnPaths">
-        <h1 class="display-1 text-center my-6">
-          Phase-3
-        </h1>
-        <p class="text-body col-md-10 mx-auto" v-html="getLearnPaths['Phase-3'].subtitle" />
-      </div>
-      <div v-if="getAboutUs" class="mt-10">
-        <h1 class="mb-5 text-center sub-titles">
-          What we hope to achieve
-        </h1>
-        <p class="text-body col-md-10 mx-auto" v-html="getAboutUs.whatWeHopeToAchieve.content" />
-      </div>
+    </section>
+
+    <v-container v-if="getAboutUs" class="my-12 py-12 text-center">
+      <h1 class="my-12 sub-titles">
+        Our Goal
+      </h1>
+      <p class="text-body" v-html="getAboutUs.whatWeHopeToAchieve.content" />
     </v-container>
   </div>
 </template>
@@ -179,7 +147,7 @@ export default {
     return {
       isActive: false,
       mdiArrowDownBoldHexagonOutline,
-      message: "At the end of phase 2, trainees will interview with our partner companies."
+      message: "Companies We Had Interviews With."
     };
   },
   head: {
@@ -196,19 +164,28 @@ export default {
   methods: {
     getFullPath(publicId) {
       return `https://res.cloudinary.com/eskalate/image/upload/q_auto:best,f_auto,w_600${publicId}`;
+    },
+    evenPhase(phase){
+      const num = parseInt(phase.split("-")[1]);
+      return num % 2 === 0;
     }
   }
 };
 </script>
 
+<style lang="sass">
+
+</style>
 <style scoped>
 .sub-titles {
   font-size: 60px;
   line-height: 60px;
   font-weight: 800;
   font-family: Lato, Nunito, sans-serif;
+  text-align: center;
 }
-.display-1 {
-  font-weight: 700;
+.text-body {
+  text-align: center;
+  font-weight: 300;
 }
 </style>

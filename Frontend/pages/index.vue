19beb3b61030be59157a4b6c6747c9253b745499
Gemmechu Mohammed Hassena <gemmechumohammed@gmail.com>
2021-02-20 12:31:56 +0300

    [web] Add home page introduction to A2SV (#121)
    * [web] add home page intro
    
    * [web] change style home page intro

--- Frontend/pages/index.vue
+++ Frontend/pages/index.vue
@@ -26,12 +26,48 @@
       id="first-container"
       class="my-md-12 py-10 pa-5 pa-sm-10 text-center text-md-left"
       style="background: url('bg.svg')"
-    >
+    ><v-row class="my-md-4" style="justify-content: center;">
+        <v-col cols="12" md="12" class=" d-flex align-center">
+          <div class="text-center">
+            <h1 class="mb-8" :class="{'subheading': $vuetify.breakpoint. smAndDown, 'display-2': $vuetify.breakpoint.mdAndUp, 'my-5':$vuetify.breakpoint.smAndUp}">
+              Africa to Silicon Valley
+            </h1>
+            <p class="text-body text-center">
+              A2SV upskills high-potential university students, connects them with internship interviews at the world’s top tech companies, and create digital solutions to urgent problems in their home countries together. The program is free for students, making the opportunity available for youth who have talent but lack the means to use it.
+            </p>
+          </div>
+        </v-col>
+      </v-row>
+    <v-row class="my-md-10 " style="justify-content: center;">
+      <v-col cols="12" md="6">
+          <cld-image
+            style="border: 2px solid #d4e5ff; border-radius: 200px"
+            loading="lazy"
+            crop="scale"
+            responsive
+            fetch-format="auto"
+            quality="auto"
+            class="z-index-1 overflow-hidden "
+            public-id="https://res.cloudinary.com/dfc7snpy5/image/upload/v1613802639/a2sv/africa3_aj1i05.jpg"
+          />
+        </v-col>
+        <v-col cols="12" md="6" class="my-md-2 d-flex align-center">
+          <div class="text-center">
+            <h1 :class="{'subheading': $vuetify.breakpoint. smAndDown, 'display-2': $vuetify.breakpoint.mdAndUp, 'my-5':$vuetify.breakpoint.smAndUp}">
+              Who We Are
+            </h1>
+            <p class="text-body text-center">
+              A2SV was founded by <b>Emre Varol</b>, a competitive programmer and machine learning engineer who worked at <b>Google Research, Palantir,</b> and <b> Liftoff.</b> He moved to Ethiopia in October 2019 to train youth with just as much potential, but less opportunity  and launched A2SV through a partnership with Addis Ababa Institute of Technology, the best technical university in the country. In July 2020,<b> Burak Saglam</b>, former <b>International Math Olympiad medalist,</b> and <b>Erden Oktay</b>(MSc Tufts University), senior software engineer, joined.
+            </p>
+          </div>
+        </v-col>
+
+      </v-row>
       <v-row class="my-md-10 mt-12">
         <v-col cols="12" md="6" class="my-md-12 d-flex align-center">
           <div>
             <h1 :class="{'subheading': $vuetify.breakpoint. smAndDown, 'display-2': $vuetify.breakpoint.mdAndUp, 'my-5':$vuetify.breakpoint.smAndUp}">
-              360° training
+              360° Training
             </h1>
             <p class="text-body">
               A2SV upskills students with a 360° software engineering program
